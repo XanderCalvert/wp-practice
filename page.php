9b35062afef49ebcc0189894917b7ac4c2dddd19
Matt Calvert <64328076+XanderCalvert@users.noreply.github.com>
2021-07-20 08:09:19 +0100

    so far

--- page.php
+++ page.php
@@ -1,17 +1,15 @@
 <?php get_header(); 
 
 while(have_posts()) {
-    the_post();?>
+    the_post();
+    pageBanner(array(
+      "title" => "This is the title",
+      "subtitle" => "This is the subtitle",
+      "photo" => "https://images.unsplash.com/photo-1626244297294-c1aecb88f542?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+    ));
+    ?>
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg") ?> );"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title() ?></h1>
-      <div class="page-banner__intro">
-        <p>TODO replace me later</p>
-      </div>
-    </div>  
-  </div>
+
 
   <div class="container container--narrow page-section">
 
